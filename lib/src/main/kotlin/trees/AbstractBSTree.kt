b6d683ac92ff714e2f7f0e9b03049c79fc84ceb9
Stepan Gorlov <Stepagor105@gmail.com>
2025-03-19 22:26:43 +0300

    Changed type of the root field for more convenient interaction from the heir classes

--- lib/src/main/kotlin/trees/AbstractBSTree.kt
+++ lib/src/main/kotlin/trees/AbstractBSTree.kt
@@ -3,7 +3,7 @@ package trees
 import trees.nodes.Node
 
 public abstract class AbstractBSTree<K : Comparable<K>, V, NODE_T : Node<K, V, NODE_T>>(): Iterable<Pair<K, V>> {
-    private var root: Node<K, V, NODE_T>? = null
+    protected var root: NODE_T? = null
 
     public fun find(key: K): V? {
         return findNode(key)?.value
