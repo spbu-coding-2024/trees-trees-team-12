631d3e0b1c09af9ac4d0ec30b875d7bd656d67d8
Stepan Gorlov <Stepagor105@gmail.com>
2025-03-27 15:26:02 +0300

    feat: add left and right rotate functions

--- lib/src/main/kotlin/trees/RBTree.kt
+++ lib/src/main/kotlin/trees/RBTree.kt
@@ -11,6 +11,38 @@ class RBTree<K : Comparable<K>, V>() : AbstractBSTree<K, V, RBNode<K, V>>() {
         return node?.color ?: RBNode.Color.BLACK
     }
 
+    fun rightRotate(node: RBNode<K, V>?) {
+        val  leftChild: RBNode<K, V>? = node?.left
+        node?.left = leftChild?.right
+        node?.left?.parent = node
+        leftChild?.right = node
+        leftChild?.parent = node?.parent
+        if (root == node) {
+            root = leftChild
+        } else if (node == node?.parent?.left) {
+            node?.parent?.left = leftChild
+        } else {
+            node?.parent?.right = leftChild
+        }
+        node?.parent = leftChild
+    }
+
+    fun leftRotate(node: RBNode<K, V>?) {
+        val rightChild: RBNode<K, V>? = node?.right
+        node?.right = rightChild?.left
+        node?.right?.parent = node
+        rightChild?.left = node
+        rightChild?.parent = node?.parent
+        if (root == node) {
+            root = rightChild
+        } else if (node == node?.parent?.left) {
+            node?.parent?.left = rightChild
+        } else {
+            node?.parent?.right = rightChild
+        }
+        node?.parent = rightChild
+    }
+
     override fun delete(key: K) {
         TODO("Not yet implemented")
     }
