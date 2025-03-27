be9dbaa6de40321e18514bce25d48d78698808b5
Stepan Gorlov <Stepagor105@gmail.com>
2025-03-27 15:25:25 +0300

    feat: add function for get color of node

--- lib/src/main/kotlin/trees/RBTree.kt
+++ lib/src/main/kotlin/trees/RBTree.kt
@@ -7,6 +7,10 @@ class RBTree<K : Comparable<K>, V>() : AbstractBSTree<K, V, RBNode<K, V>>() {
         TODO("Not yet implemented")
     }
 
+    private fun getColor(node: RBNode<K, V>?): RBNode.Color {
+        return node?.color ?: RBNode.Color.BLACK
+    }
+
     override fun delete(key: K) {
         TODO("Not yet implemented")
     }
