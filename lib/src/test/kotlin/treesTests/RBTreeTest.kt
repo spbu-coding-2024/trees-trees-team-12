3c9e9fd6d954798d5fc5f9134204e61c01521fe6
Stepan Gorlov <Stepagor105@gmail.com>
2025-03-31 22:34:38 +0300

    test: add binary search tree validation, add test cases, fix tests naming

--- lib/src/test/kotlin/treesTests/RBTreeTest.kt
+++ lib/src/test/kotlin/treesTests/RBTreeTest.kt
@@ -11,42 +11,45 @@ class RBTreeTest {
     private lateinit var tree: RBTree<Int, Int>
     @BeforeEach
     fun setUp() {
-        tree = RBTree<Int, Int>()
+        tree = RBTree()
     }
 
     @Test
-    fun `insert_one_node`() {
+    fun `insert one node`() {
         tree.insert(1, 1)
         assertEquals(tree.root?.color, RBNode.Color.BLACK)
         assertEquals(tree.root?.key, 1)
         assertEquals(treeSize(tree), 1)
         assertEquals(tree.find(1), 1)
+        assertTrue(isBinarySearchTree(tree.root))
     }
 
     @Test
-    fun `insert_ten_increasing_nums`() {
+    fun `insert ten increasing nums`() {
         for (i in 1 .. 10) {
             tree.insert(i, i)
             checkInvariant(tree.root)
             assertEquals(tree.find(i), i)
             assertEquals(tree.find(i + 1), null)
+            assertTrue(isBinarySearchTree(tree.root))
         }
         assertEquals(treeSize(tree), 10)
     }
 
     @Test
-    fun `insert_20_decreasing_numbers`() {
+    fun `insert 20 decreasing numbers`() {
         for (i in 20 downTo 1) {
             tree.insert(i, i)
             checkInvariant(tree.root)
             assertEquals(tree.find(i), i)
             assertEquals(tree.find(i - 1), null)
+            assertTrue(isBinarySearchTree(tree.root))
         }
         assertEquals(treeSize(tree), 20)
     }
 
     @Test
-    fun `insert_20_shuffled_numbers_with_delete`() {
+    fun `insert 20 shuffled numbers with_delete`() {
         var shuffled = (1 .. 20).shuffled()
         var size = 0
         for (i in shuffled) {
@@ -55,6 +58,7 @@ class RBTreeTest {
             checkInvariant(tree.root)
             assertEquals(tree.find(i), i)
             assertEquals(treeSize(tree), size)
+            assertTrue(isBinarySearchTree(tree.root))
         }
         assertEquals(treeSize(tree), 20)
 
@@ -66,30 +70,51 @@ class RBTreeTest {
             checkInvariant(tree.root)
             assertEquals(tree.find(i), null)
             assertEquals(treeSize(tree), size)
+            assertTrue(isBinarySearchTree(tree.root))
         }
     }
 
+    @Test
+    fun `delete from empty tree`() {
+        tree.delete(1)
+    }
+
+    @Test
+    fun `delete node that is not in the tree`() {
+        for (i in 1 .. 10) {
+            tree.insert(i, i)
+        }
+        checkInvariant(tree.root)
+        assertEquals(treeSize(tree), 10)
+        tree.delete(11)
+        checkInvariant(tree.root)
+        assertEquals(treeSize(tree), 10)
+    }
+
     @RepeatedTest(100)
-    fun `big_find_test`() {
+    fun `big find test`() {
         var shuffled = (1 .. 10000).shuffled().take(1000)
         for (i in shuffled) {
             tree.insert(i, i)
+            assertTrue(isBinarySearchTree(tree.root))
         }
 
         shuffled = shuffled.shuffled()
 
         for (i in shuffled) {
             assertEquals(tree.find(i), i)
+            assertTrue(isBinarySearchTree(tree.root))
         }
     }
 
     @Test
-    fun `small_delete_test`() {
+    fun `small delete test`() {
         for (i in 1 .. 100) {
             tree.insert(i, i)
             assertEquals(tree.find(i), i)
             assertEquals(treeSize(tree), i)
             checkInvariant(tree.root)
+            assertTrue(isBinarySearchTree(tree.root))
         }
 
         for (i in 100 downTo 1) {
@@ -97,16 +122,18 @@ class RBTreeTest {
             assertEquals(tree.find(i), null)
             assertEquals(treeSize(tree), i - 1)
             checkInvariant(tree.root)
+            assertTrue(isBinarySearchTree(tree.root))
         }
     }
 
-    @RepeatedTest(20)
-    fun `large_property_test`() {
+    @RepeatedTest(10)
+    fun `large property test`() {
         var shuffled = (1 .. 100000).shuffled().take(10000)
         for (i in shuffled) {
             tree.insert(i, i)
             assertEquals(tree.find(i), i)
             checkInvariant(tree.root)
+            assertTrue(isBinarySearchTree(tree.root))
         }
 
         assertEquals(treeSize(tree), 10000)
@@ -117,16 +144,17 @@ class RBTreeTest {
             tree.delete(i)
             assertEquals(tree.find(i), null)
             checkInvariant(tree.root)
+            assertTrue(isBinarySearchTree(tree.root))
         }
         assertEquals(treeSize(tree), 0)
     }
 
-    fun checkInvariant(node: RBNode<Int, Int>?): Int {
+    private fun checkInvariant(node: RBNode<Int, Int>?): Int {
         if (node == null) return 1
 
         if (node.color == RBNode.Color.RED) {
-            assertNotEquals(node.color, node.left?.color)
-            assertNotEquals(node.color, node.right?.color)
+            assertNotEquals(node.color, getNodeColor(node.left))
+            assertNotEquals(node.color, getNodeColor(node.right))
         }
 
         val leftHeight: Int = checkInvariant(node.left)
@@ -137,16 +165,39 @@ class RBTreeTest {
         return if (node.color == RBNode.Color.BLACK) leftHeight + 1 else leftHeight
     }
 
-    internal fun getNodeColor(node: RBNode<Int, Int>?): RBNode.Color{
+    private fun getNodeColor(node: RBNode<Int, Int>?): RBNode.Color{
         return node?.color ?: RBNode.Color.BLACK
     }
 
-    fun treeSize(tree: RBTree<Int, Int>): Int {
+    private fun treeSize(tree: RBTree<Int, Int>): Int {
         return subTreeSize(tree.root)
     }
 
-    fun subTreeSize(node: RBNode<Int, Int>?): Int {
+    private fun subTreeSize(node: RBNode<Int, Int>?): Int {
         if (node == null) return 0
         return 1 + subTreeSize(node.left) + subTreeSize(node.right)
+    }
+
+    private fun isBinarySearchTree(node: RBNode<Int, Int>?): Boolean {
+        if (node == null) return true
+        return isBinarySearchTree(node.left) && isBinarySearchTree(node.right) &&
+                (node.key >= findMaxKey(node.left)) &&
+                (node.key <= findMinKey(node.right))
+    }
+
+    private fun findMinKey(node: RBNode<Int, Int>?): Int {
+        var current: RBNode<Int, Int>? = node
+        while (current?.left != null) {
+            current = current.left
+        }
+        return current?.key ?: Int.MAX_VALUE
+    }
+
+    private fun findMaxKey(node: RBNode<Int, Int>?): Int {
+        var current: RBNode<Int, Int>? = node
+        while (current?.right != null) {
+            current = current.right
+        }
+        return current?.key ?: Int.MIN_VALUE
     }
 }
