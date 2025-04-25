cc06dc2cf10398781254dfb4e58652ddd7781b4d
Daniil Chepurnikh <chepurnikh.daniil@gmail.com>
2025-04-25 22:19:48 +0300

    test: add new tests
    Signed-off-by: Daniil Chepurnikh <chepurnikh.daniil@gmail.com>

--- lib/src/test/kotlin/treesTests/BSTreeTest.kt
+++ lib/src/test/kotlin/treesTests/BSTreeTest.kt
@@ -1,25 +1,21 @@
 package treesTests
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 import trees.BSTree
-import trees.nodes.RBNode
 
 class BSTreeTest
 {
     private lateinit var tree: BSTree<Int, Int>
 
     @BeforeEach
-    fun setUp()
-    {
+    fun setUp() {
         tree = BSTree()
     }
 
     @Test
-    fun `insert one node`()
-    {
+    fun `insert one node`() {
         tree.insert(1, 1)
         assertEquals(tree.root?.key, 1)
         assertEquals(tree.root?.value, 1)
@@ -27,10 +23,8 @@ class BSTreeTest
     }
 
     @Test
-    fun `insert one node with different values`()
-    {
-        for(p in 1..5)
-        {
+    fun `insert one node with different values`() {
+        for (p in 1..5) {
             tree.insert(1, p)
             assertEquals(tree.root?.key, 1)
             assertEquals(tree.root?.value, p)
@@ -38,5 +32,25 @@ class BSTreeTest
         }
     }
 
+    @Test
+    fun `insert 111 nodes`() {
+        for (i in 1..111) {
+            tree.insert(i, i)
+            assertEquals(tree.find(i), i)
+            assertEquals(tree.find(i - 1), null)
+        }
+    }
 
+    @Test
+    fun `delete from empty tree`() {
+        tree.delete(1)
+    }
+
+    @Test
+    fun `delete non-existent node `() {
+        tree.insert(1, 1)
+        tree.delete(2)
+    }
 }
+
+
